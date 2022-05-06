8aaedfdf3b7a54455fe7da0ff242ec48bbb4393e
a10411 <93932321+a10411@users.noreply.github.com>
2022-05-06 16:26:52 +0100

    marcarservicov2

--- MarcarServico/functions.js
+++ MarcarServico/functions.js
@@ -1,13 +1,29 @@
-let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector(".bx-search");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
+  let iconBtn = document.querySelector(".li.a");
 
-btn.onclick = function(){
-    sidebar.classList.toggle("active");
+  closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+  });
 
-}
+  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+  });
 
-searchBtn.onclick = function(){
-    sidebar.classList.toggle("active");
-    
-}
+  iconBtn.addEventListener("click",()=>{
+    iconBtn.classList.toggle("pressed");
+    optionPressed(); //makes the colour change for the selected option
+
+  });
+
+  // following are the code to change sidebar button(optional)
+  function menuBtnChange() {
+   if(sidebar.classList.contains("open")){
+     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
